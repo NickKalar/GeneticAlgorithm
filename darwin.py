7a95ebd9c62acb84204d099c542001ed70bba45d
Nicholas <16458785+NickKalar@users.noreply.github.com>
2019-11-26 20:40:08 -0800

    Updated every function. Added formatting for outputs and made the program slightly more dynamic

--- darwin.py
+++ darwin.py
@@ -1,21 +1,16 @@
 from random import random
 from random import seed
+from random import randrange
 
 seed(None, 2)
-
-term = False
-pop = [[], [], [], [], [], [], [], [], [], []]
-
-for x in range(10):
-    for y in range(20):
-        i = 0
-        if random() > .5:
-            i = 1
-        pop[x].append(i)
+# total number of "genes" per individuals
+genome = 20
+# total number of generations to be ran
+generations = 10
 
 
-def control(countdown):
-    while countdown != 0 and not term:
+def control(countdown, pop):
+    while countdown < generations:
         newpop = []
         for p in pop:
             if random() > .5:
@@ -28,47 +23,79 @@ def control(countdown):
         newpop = elitism(pop, newpop)
         pop = newpop
 
+        print(f"Epoch {countdown+1}: ")
 
+        individual = 1
+        for p in pop:
+            fitness = 0
+            for i in p:
+                if i == 1:
+                    fitness += 1
+            print(f"Individual {individual}: {p} Fitness {fitness}")
+            individual += 1
+        countdown += 1
+
+
+# randomly selects an individual from the population to be used later
 def selection(popSelect):
-    rand = random() * 10
+    rand = randrange(0, len(popSelect))
     return popSelect[int(rand)]
 
 
+# creates two new individuals from "crossing" the genome of the "father" and "mother"
 def crossover(father, mother):
     first = []
     last = []
-    for j in range(5):
-        first.append(mother[j])
-        last.append(father[j])
-    for j in range(5, 10):
-        last.append(mother[j])
-        last.append(father[j])
-    return father, mother
+    for j in range(genome):
+        if genome < 10:
+            first.append(mother[j])
+            last.append(father[j])
+        else:
+            last.append(mother[j])
+            first.append(father[j])
+    return first, last
 
 
+# given a 10% chance to mutate any "gene"
 def mutate(ind):
     for i in ind:
-        if random() < .1:
-            if ind[i] == 0:
-                ind[i] = 1
+        rand = randrange(0, 9)
+        if rand == 1:
+            if i == 0:
+                i = 1
             else:
-                ind[i] = 0
+                i = 0
     return ind
 
 
+# not technically an evolutionary thing, but it picks the best individual from
+# the last generation and returns to to ensure it is in the next generation
 def elitism(p, np):
     global best
     for n in p:
         q = 0
-        if sum(p) > q:
-            best = p
+        t = 0
+        for i in n:
+            if i == 1:
+                t += 1
+        if t > q:
+            best = n
     np.append(best)
     return np
 
 
 def main():
-    countdown = 10
-    control(countdown)
+    countdown = 0
+    pop = [[], [], [], [], [], [], [], [], [], []]
+
+    for x in range(10):
+        for y in range(20):
+            i = 0
+            if random() > .5:
+                i = 1
+            pop[x].append(i)
+
+    control(countdown, pop)
 
 
 main()

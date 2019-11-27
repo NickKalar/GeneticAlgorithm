f968b3852fda09d3b650e8b09ff18126374adab5
Nicholas <16458785+NickKalar@users.noreply.github.com>
2019-11-27 09:25:00 -0800

    Refactored code for better readability. Added comments to functions.

--- darwin.py
+++ darwin.py
@@ -10,18 +10,21 @@ generations = 10
 
 
 def control(countdown, pop):
-    while countdown < generations:
-        newpop = []
+    populationFitness = 0
+    # executes for a certain number of generations
+    # or until an individual has a fitness of 20
+    while countdown < generations and populationFitness != 20:
+        newPop = []
         for p in pop:
             if random() > .5:
-                c = selection(pop)
-                (a, b) = crossover(p, c)
-                a = mutate(a)
-                b = mutate(b)
-                newpop.append(a)
-                newpop.append(b)
-        newpop = elitism(pop, newpop)
-        pop = newpop
+                selected = selection(pop)
+                (child1, child2) = crossover(p, selected)
+                child1 = mutate(child1)
+                child2 = mutate(child2)
+                newPop.append(child1)
+                newPop.append(child2)
+        newPop = elitism(pop, newPop)
+        pop = newPop
 
         print(f"Epoch {countdown+1}: ")
 
@@ -33,7 +36,14 @@ def control(countdown, pop):
                     fitness += 1
             print(f"Individual {individual}: {p} Fitness {fitness}")
             individual += 1
+
+            # stores the value for the "fittest" individual in this generation
+            if fitness > populationFitness:
+                populationFitness = fitness
+
         countdown += 1
+    if populationFitness == 20:
+        print("We have a winner!")
 
 
 # randomly selects an individual from the population to be used later
@@ -44,52 +54,54 @@ def selection(popSelect):
 
 # creates two new individuals from "crossing" the genome of the "father" and "mother"
 def crossover(father, mother):
+    # "children" arrays
     first = []
-    last = []
+    second = []
     for j in range(genome):
-        if genome < 10:
+        if genome < genome/2:
             first.append(mother[j])
-            last.append(father[j])
+            second.append(father[j])
         else:
-            last.append(mother[j])
+            second.append(mother[j])
             first.append(father[j])
-    return first, last
+    return first, second
 
 
-# given a 10% chance to mutate any "gene"
+# given a 10% chance to mutate a "gene"
 def mutate(ind):
-    for i in ind:
-        rand = randrange(0, 9)
-        if rand == 1:
-            if i == 0:
-                i = 1
-            else:
-                i = 0
+    rand = randrange(0, 9)
+    if rand == 3:
+        i = randrange(0, 19)
+        if ind[i] == 0:
+            ind[i] = 1
+        else:
+            ind[i] = 0
     return ind
 
 
 # not technically an evolutionary thing, but it picks the best individual from
 # the last generation and returns to to ensure it is in the next generation
-def elitism(p, np):
+def elitism(population, newPopulation):
     global best
-    for n in p:
-        q = 0
+    quality = 0
+    for individual in population:
         t = 0
-        for i in n:
+        for i in individual:
             if i == 1:
                 t += 1
-        if t > q:
-            best = n
-    np.append(best)
-    return np
+        if t > quality:
+            quality = t
+            best = individual
+    newPopulation.append(best)
+    return newPopulation
 
 
 def main():
     countdown = 0
     pop = [[], [], [], [], [], [], [], [], [], []]
 
-    for x in range(10):
-        for y in range(20):
+    for x in range(len(pop)):
+        for y in range(genome):
             i = 0
             if random() > .5:
                 i = 1
